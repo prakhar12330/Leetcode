d3d59136d668a4bc2b2ae54dd510f56887df9175
Prakhar Singh <84180549+prakhar12330@users.noreply.github.com>
2022-01-05 16:14:44 +0530

    Time: 70 ms (6.31%), Space: 121.3 MB (11.52%) - LeetHub

---
+++ 38-count-and-say/38-count-and-say.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string countAndSay(int n)
+    {
+       if(n==1)
+       {
+           return "1";
+       }
+       string s=countAndSay(n-1);
+       int counter=0;
+       string rst="";
+       for(int i=0;i<s.size();i++)
+       {
+          counter++;
+          if( i ==(s.size()-1) || s[i]!=s[i+1]  )
+          {
+              rst=rst+to_string(counter)+s[i];
+              counter=0;
+          }
+       }
+       return rst;
+    }
+};
