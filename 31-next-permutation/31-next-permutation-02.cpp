8f07cc2d0e02e3a39915f11d0f0024f649a8e4ee
Prakhar Singh <84180549+prakhar12330@users.noreply.github.com>
2022-01-10 21:26:50 +0530

    Create 31-next-permutation-02.cpp

---
+++ 31-next-permutation/31-next-permutation-02.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums)
+    {
+        
+        int idx1=-1;
+        int idx2=-1;
+        
+        if(nums.size()==1) return;
+        for(int i=nums.size()-2;i>=0;i--)
+        {
+          if(nums[i]<nums[i+1])
+          {
+              idx1=i;
+              break;
+          }
+        }
+        if(idx1==-1)
+        {
+            reverse(nums.begin(),nums.end());
+            return;
+        }
+        else
+        {
+        for(int i=nums.size()-1;i>=0;i--)
+        {
+          if(nums[idx1]<nums[i])
+          {
+              idx2=i;
+              break;
+          }
+        }
+        swap(nums[idx1],nums[idx2]);
+        sort(nums.begin()+idx1+1,nums.end());
+        }
+        
+    }
+};
