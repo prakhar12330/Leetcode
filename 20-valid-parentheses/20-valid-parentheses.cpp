4a1ca83599934d4ff551833f623f5250d7359943
Prakhar Singh <84180549+prakhar12330@users.noreply.github.com>
2022-01-10 23:34:39 +0530

    Time: 5 ms (8.16%), Space: 6.4 MB (9.57%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isValid(string s) 
+    {
+        stack<char> st;
+        for(int i=0;i<s.size();i++)
+        {
+           if(s[i]=='(' or s[i]=='[' or s[i]=='{')
+           {
+               st.push(s[i]);
+           }
+           else if(st.empty() or (st.top()=='(' and s[i]!=')') or                      (st.top()=='{' and s[i]!='}') or (st.top()=='[' and s[i]!=']'))
+           {
+               return false;
+           }
+            else
+            {
+              st.pop();  
+            }
+        }
+        return st.empty();
+    }
+};
