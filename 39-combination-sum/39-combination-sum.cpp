6d5ffd4382f11ab6589130ac8ffdab791296b601
Prakhar Singh <84180549+prakhar12330@users.noreply.github.com>
2022-01-25 23:34:21 +0530

    Time: 18 ms (41.34%), Space: 10.7 MB (88.99%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,27 +1,33 @@
 class Solution {
-public: 
-    void findCombination(int ind, int target, vector<int> &arr, vector<vector<int>> &ans, vector<int>&ds) {
-        if(ind == arr.size()) {
-            if(target == 0) {
-                ans.push_back(ds); 
-            }
-            return; 
-        }
-        // pick up the element 
-        if(arr[ind] <= target) {
-            ds.push_back(arr[ind]); 
-            findCombination(ind, target - arr[ind], arr, ans, ds); 
-            ds.pop_back(); 
-        }
-        
-        findCombination(ind+1, target, arr, ans, ds); 
-        
-    }
 public:
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>> ans; 
-        vector<int> ds; 
-        findCombination(0, target, candidates, ans, ds); 
-        return ans; 
+    
+    void formCombination(int idx,vector<int>& candidates,int target, vector<int>&ds,vector<vector<int>> &ans)
+    {
+       //Base Case
+        if(idx>=candidates.size())
+        {
+            if(target==0)
+            {
+                ans.push_back(ds);
+            }
+            return;
+        }
+        //picking condition
+        if(target>=candidates[idx])
+        {
+        ds.push_back(candidates[idx]);
+        formCombination(idx,candidates,target-candidates[idx],ds,ans);
+        ds.pop_back();
+        }
+        //not picking condition
+        formCombination(idx+1,candidates,target,ds,ans);
+       
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target)
+    {
+        vector<vector<int>>rst;
+        vector<int>ds;
+        formCombination(0,candidates,target,ds,rst);
+        return rst;
     }
 };
