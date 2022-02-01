96fd89fb844a427e81a124301ae48ec690cabd88
Prakhar Singh <84180549+prakhar12330@users.noreply.github.com>
2022-02-01 23:41:58 +0530

    Time: 7 ms (35.54%), Space: 11.2 MB (28.95%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,35 +1,39 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int l = 0, r = nums.size()-1;
-        while (l <= r) 
+    int search(vector<int>& nums, int target)
+    {
+        int lo=0;
+        int hi=nums.size()-1;
+        while(lo<=hi)
         {
-            int mid = (l+r) / 2;
-            if (target == nums[mid])
+            int mid=(lo+hi)/2;
+            if(nums[mid]==target)
+            {
                 return mid;
-            // there exists rotation; the middle element is in the left part of the array
-            if (nums[mid] > nums[r]) 
+            }
+            //if the left side is sorted //
+            if(nums[lo]<=nums[mid])
             {
-                if (target < nums[mid] && target >= nums[l])
-                    r = mid - 1;
+                if(target>=nums[lo] && target <=nums[mid])
+                {
+                    hi=mid-1;
+                }
                 else
-                    l = mid + 1;
+                {
+                    lo=mid+1;
+                }
             }
-            // there exists rotation; the middle element is in the right part of the array
-            else if (nums[mid] < nums[l]) 
+            if(nums[hi]>=nums[mid])
             {
-                if (target > nums[mid] && target <= nums[r])
-                    l = mid + 1;
+                if(target>=nums[mid] && target <=nums[hi])
+                {
+                    lo=mid+1;
+                }
                 else
-                    r = mid - 1;
+                {
+                    hi=mid-1;
+                }
             }
-             else {
-                if (target < nums[mid])
-                    r = mid - 1;
-                else
-                    l = mid + 1;
-            }
-            
         }
         return -1;
     }
