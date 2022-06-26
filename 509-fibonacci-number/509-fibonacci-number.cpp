c9ad2eff37c4f6db9931eabecfffc67b58572618
Prakhar Singh <84180549+prakhar12330@users.noreply.github.com>
2022-06-26 10:29:46 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (9.60%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,21 +1,16 @@
-// Top-Down Approach
+// Bottom-Up Approach
 class Solution {
 public:
-    int recurrFib(int n,vector<int>& dp)
-    {
-       if(n==0 || n==1)return n; //Base Case 
-        
-       if(dp[n]!=-1)return dp[n]; 
-       dp[n]=recurrFib(n-1,dp)+recurrFib(n-2,dp);
-       return dp[n];
-    }
     int fib(int n)
     {
         vector<int>dp(n+1);
-        for(int i=0;i<n+1;i++)
+        if(n<2)return n;
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2;i<n+1;i++)
         {
-            dp[i]=-1;
+            dp[i]=dp[i-1]+dp[i-2];
         }
-        return recurrFib(n,dp);  
+        return dp[n];  
     }
 };
