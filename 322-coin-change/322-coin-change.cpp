33794e08032eaade0492e332f529191fac9e15dd
Prakhar Singh <84180549+prakhar12330@users.noreply.github.com>
2022-06-28 11:42:22 +0530

    Time: 164 ms (32.45%), Space: 17.3 MB (31.81%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -10,7 +10,7 @@ public:
         if(amount<0) return INT_MAX;
         
         int mini=INT_MAX;
-        
+        //Step03
         if(dp[amount]!=-1)return dp[amount];
         
         for(int i=0;i<coins.size();i++)
@@ -20,13 +20,15 @@ public:
             {
             mini=min(mini,1+ans);
             }
-        }
-        dp[amount]=mini;
+        }   
+        //Step02-memoization
+        dp[amount]=mini; 
         return mini;
         
     }
     int coinChange(vector<int>& coins, int amount) 
     {
+        //Step01-Created the dp array
         vector<int>dp(amount+1,-1);
        int ans=solveRecur(coins,amount,dp);
        if(ans==INT_MAX)
