658d141360f579b5ce1c856e0ce9bc76af3ce15e
Prakhar Singh <84180549+prakhar12330@users.noreply.github.com>
2022-06-28 12:24:40 +0530

    Time: 180 ms (26.02%), Space: 14.5 MB (57.07%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,39 +1,29 @@
-//Top Down Approach
+//Bottom Up Approach
 class Solution {
 public:
     
-    
-    int solveRecur(vector<int>& coins,int amount,vector<int>& dp)
+    int solveRecur(vector<int>& coins,int amount)
     {
-        //Base Case
-        if(amount==0)return 0;
-        if(amount<0) return INT_MAX;
-        
-        int mini=INT_MAX;
-        //Step03
-        if(dp[amount]!=-1)return dp[amount];
-        
-        for(int i=0;i<coins.size();i++)
+        //Step01-Base Case Analysis
+        vector<int>dp(amount+1,INT_MAX);
+        dp[0]=0;
+        for(int i=1;i<amount+1;i++)//for making a particular amount we have a three choices
         {
-            int ans=solveRecur(coins,amount-coins[i],dp);
-            if(ans!=INT_MAX)
-            {
-            mini=min(mini,1+ans);
-            }
-        }   
-        //Step02-memoization
-        dp[amount]=mini; 
-        return mini;
-        
+          for(int j=0;j<coins.size();j++) 
+          {
+              if(i-coins[j]>=0 && dp[i-coins[j]] !=INT_MAX)
+              {
+              dp[i]=min(dp[i],1+dp[i-coins[j]]);
+              }
+          }
+        }
+        if(dp[amount]==INT_MAX)return -1;
+        else return dp[amount];
     }
     int coinChange(vector<int>& coins, int amount) 
     {
-        //Step01-Created the dp array
-        vector<int>dp(amount+1,-1);
-       int ans=solveRecur(coins,amount,dp);
-       if(ans==INT_MAX)
-          return -1;
-       else 
-          return ans;
+    
+       return solveRecur(coins,amount);
+       
     }
 };
