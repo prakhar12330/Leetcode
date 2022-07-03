09cb5691c77558c56e010e80c5274e5451cfbe7d
Prakhar Singh <84180549+prakhar12330@users.noreply.github.com>
2022-07-03 17:11:33 +0530

    Time: 4 ms (34.79%), Space: 7.8 MB (47.12%) - LeetHub

---
+++ 198-house-robber/198-house-robber.cpp
@@ -0,0 +1,28 @@
+//Top Down Approach
+class Solution {
+public:
+    
+    int solve(vector<int>& nums,int n,vector<int>&dp)
+    {
+        //Base Case
+        if(n<0)return 0;
+        if(n==0)return nums[0];
+        
+        if(dp[n]!=-1)return dp[n];
+        
+        int incl=solve(nums,n-2,dp)+nums[n];
+        int excl=solve(nums,n-1,dp)+0;
+        
+        dp[n]= max(incl,excl);
+        
+        return dp[n];
+        
+    }
+    int rob(vector<int>& nums) 
+    {
+        int n=nums.size();
+        vector<int>dp(n,-1); //Step01
+        int ans =solve(nums,n-1,dp);// n-1 indicates the last index as we will be traversing from end 
+        return ans;
+    }
+};
