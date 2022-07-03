7e7a565dfe3c7810a8c6f64b7eb4ac9d58bd31d3
Prakhar Singh <84180549+prakhar12330@users.noreply.github.com>
2022-07-03 17:34:55 +0530

    Time: 3 ms (53.93%), Space: 7.9 MB (5.96%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,27 +1,27 @@
-//Top Down Approach
+//Bottom Up Approach
 class Solution {
 public:
     
-    int solve(vector<int>& nums,int n,vector<int>&dp)
-    {
-        //Base Case
-        if(n<0)return 0;
-        if(n==0)return nums[0];
-        
-        if(dp[n]!=-1)return dp[n];//Step03
-        
-        int incl=solve(nums,n-2,dp)+nums[n];
-        int excl=solve(nums,n-1,dp)+0;
-        
-        dp[n]= max(incl,excl); //Step02
-        return dp[n];
-        
-    }
+    
     int rob(vector<int>& nums) 
     {
         int n=nums.size();
-        vector<int>dp(n,-1); //Step01
-        int ans =solve(nums,n-1,dp);// n-1 indicates the last index as we will be traversing from end 
-        return ans;
+        vector<int>dp(n,0);//Step01
+        if(n==0) return 0;
+        if(n==1) return nums[0];
+        if(n==2) return max(nums[0],nums[1]);
+        
+        dp[0]=nums[0];//Step02
+        dp[1] = max(nums[0],nums[1]);
+        
+        //Step03
+        for(int i=2;i<n;i++)
+        {
+           
+            int incl=dp[i-2]+nums[i];
+            int excl=dp[i-1];
+            dp[i]=max(incl,excl);
+        }
+        return dp[n-1];
     }
 };
