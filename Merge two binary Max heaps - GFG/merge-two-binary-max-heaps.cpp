8333b87d8aeb44bedd0b4923029f25c67bdda6b0
Prakhar Singh <84180549+prakhar12330@users.noreply.github.com>
2022-07-15 12:24:20 +0530

    Added solution - LeetHub

--- Merge two binary Max heaps - GFG/merge-two-binary-max-heaps.cpp
+++ Merge two binary Max heaps - GFG/merge-two-binary-max-heaps.cpp
@@ -10,29 +10,45 @@ using namespace std;
  // } Driver Code Ends
 // User function Template for C++
 
-//Using Priority Queue
 class Solution{
     public:
-    vector<int> mergeHeaps(vector<int> &a, vector<int> &b, int n, int m)
-    {
+    
+    void heapify(vector<int>&arr,int size,int i){
+		int largest=i;
+		int left=2*i+1;
+		int right=2*i+2;
+		if(left<size&&arr[largest]<arr[left]){
+			largest=left;
+			
+		}
+		if(right<size&&arr[largest]<arr[right]){
+			largest=right;
+			
+		}
+		if(largest!=i){
+			swap(arr[largest],arr[i]);
+			heapify(arr,size,largest);
+		}
+	}
+    vector<int> mergeHeaps(vector<int> &a, vector<int> &b, int n, int m) {
         // your code here
-        vector<int>result;
-        priority_queue<int>pq;
-        for(int i=0;i<n;i++)
-        {
-          pq.push(a[i]);
-        }
-        for(int i=0;i<m;i++)
-        {
-          pq.push(b[i]);
-        }
-         while(!pq.empty())
-        {
-            result.push_back(pq.top());
-            pq.pop();
-        }
+        //step 1:merge both array into one
+        vector<int>ans;
+        for(auto i:a)
+           ans.push_back(i);
+        for(auto i:b)
+          ans.push_back(i);
         
-        return result;
+        
+        
+        //step 2: build heap using merged array
+        int q=ans.size();
+        for(int i=q/2-1;i>=0;i--){
+    	heapify(ans,q,i);
+    	
+	}
+	return ans;
+        
     }
 };
 
