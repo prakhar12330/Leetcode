1c0f41a9bf134040f9ea6b21698e57c34300ac37
Prakhar Singh <84180549+prakhar12330@users.noreply.github.com>
2022-07-28 22:02:57 +0530

    Time: 19 ms (36.65%), Space: 7.2 MB (77.97%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,20 +1,10 @@
 class Solution {
 public:
-    bool isAnagram(string s, string t)
+    bool isAnagram(string s, string t) 
     {
-        //Edge Case
-        //if the size of both the array are not the same then directrly return false;
-        if(s.size()!=t.size())return false;
-        int count[26]={0};
-        for(int i=0;i<s.size();i++)
-        {
-            count[s[i]-'a']++;
-            count[t[i]-'a']--;
-        }
-        for (int i = 0; i < 26; i++)
-            if (count[i]) return false;
-        return true;
-        
+      sort(s.begin(),s.end());
+      sort(t.begin(),t.end());
+      return s==t;  
         
     }
 };
