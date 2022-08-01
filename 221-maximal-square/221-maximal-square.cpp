0d59321c553939785567275a47d257196d57b83e
Prakhar Singh <84180549+prakhar12330@users.noreply.github.com>
2022-08-01 23:18:51 +0530

    Time: 137 ms (23.25%), Space: 18.1 MB (65.32%) - LeetHub

--- 221-maximal-square/221-maximal-square.cpp
+++ 221-maximal-square/221-maximal-square.cpp
@@ -1,40 +1,40 @@
+//Bottom Up Approach
 class Solution {
 public:
     
-    int solveRecurr(vector<vector<char>>& matrix,int i,int j,int& maxArea,vector<vector<int>>&dp)
-    {
-       int rowSize=matrix.size();
-       int colSize=matrix[0].size();
-       //Base Case
-       //if The row and col pointer moves out of the matrix
+     int solveTab(vector<vector<char>>& matrix,int &maxArea)
+     {
+       int n=matrix.size();
+       int m=matrix[0].size();
+       vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+       for(int i=n-1;i>=0;i--)
+       {
+           for(int j=m-1;j>=0;j--)
+           {
+               int right=dp[i][j+1];
+               int diagonal=dp[i+1][j+1];
+               int down=dp[i+1][j];
         
+               if(matrix[i][j]=='1')
+                {
+                dp[i][j]=1+min(right,min(diagonal,down));
+                maxArea=max(maxArea,dp[i][j]*dp[i][j]); 
+                }
+                else
+                {
+                   dp[i][j]=0;
+                }
+           }
+       }
+       return dp[0][0];
        
-       if(i>=rowSize || j>=colSize)return 0;
-       if(dp[i][j]!=-1)return dp[i][j];
-       
-       int right=solveRecurr(matrix,i,j+1,maxArea,dp);//right side me kitne square form ho skte hai current element pe khade ho kr
-       int diagonal=solveRecurr(matrix,i+1,j+1,maxArea,dp);//diagonal side me kitne square form ho skte hai current element pe khade ho kr
-       int down=solveRecurr(matrix,i+1,j,maxArea,dp);//down side me kitne square form ho skte hai current element pe khade ho kr
-        
-        if(matrix[i][j]=='1')
-        {
-            dp[i][j]=1+min(right,min(diagonal,down));//sabhi possible squares ki side ayegi isme+++
-            maxArea=max(maxArea,dp[i][j]*dp[i][j]); //max me store karate rahege compoare krke ans sw
-            return dp[i][j];
-            
-        }
-        else
-        {
-            return dp[i][j]=0;
-        }
     }
     int maximalSquare(vector<vector<char>>& matrix)
     {
        int maxArea=0;
-       int row=matrix.size();
-       int col=matrix[0].size();
-       vector<vector<int>>dp(row,vector<int>(col,-1));
-       solveRecurr(matrix,0,0,maxArea,dp);
+       int n=matrix.size();
+       int m=matrix[0].size();
+       solveTab(matrix,maxArea);
        return maxArea;
        
     }
