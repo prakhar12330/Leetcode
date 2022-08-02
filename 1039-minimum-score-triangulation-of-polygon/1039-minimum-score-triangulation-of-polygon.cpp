6f90bc02e125ee70225f50d33702618f83f595e2
Prakhar Singh <84180549+prakhar12330@users.noreply.github.com>
2022-08-02 18:23:02 +0530

    Time: 4 ms (96.40%), Space: 8.6 MB (15.88%) - LeetHub

--- 1039-minimum-score-triangulation-of-polygon/1039-minimum-score-triangulation-of-polygon.cpp
+++ 1039-minimum-score-triangulation-of-polygon/1039-minimum-score-triangulation-of-polygon.cpp
@@ -1,23 +1,26 @@
-//Top Down Approach
+//Bottom Up Approach
 class Solution {
 public:
-    int solveMem(vector<int>& values,int i,int j,vector<vector<int>>&dp)
+    int solveTab(vector<int>& values)
     {
-        //Base Case
-        if(i+1==j)return 0;
-        int ans=INT_MAX;
-        if(dp[i][j]!=-1)return dp[i][j];
-        for(int k=i+1;k<j;k++)
+        int n=values.size();
+        vector<vector<int>>dp(n,vector<int>(n,0));
+        for(int i=n-1;i>=0;i--)
         {
-           ans=min(ans,values[i]*values[j]*values[k]+solveMem(values,i,k,dp)+solveMem(values,k,j,dp)); 
+            for(int j=i+2;j<n;j++)
+            {
+                int ans=INT_MAX;
+                for(int k=i+1;k<j;k++)
+                 {
+                    ans=min(ans,values[i]*values[j]*values[k]+dp[i][k]+dp[k][j]); 
+                 }
+                 dp[i][j]=ans;
+            }
         }
-        dp[i][j]=ans;
-        return  dp[i][j];
+        return dp[0][n-1];//starting se yahi goal tha ki i ko zero pe rakh kr ar j ko n-1 pr rakh kr ans nikalo
     }
     int minScoreTriangulation(vector<int>& values) 
     {
-        int n=values.size();
-        vector<vector<int>>dp(n,vector<int>(n,-1));
-        return solveMem(values,0,n-1,dp);
+        return solveTab(values);
     }
 };
