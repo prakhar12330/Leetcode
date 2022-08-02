b33cd507ad5445ccf3c38748045de83311f1344b
Prakhar Singh <84180549+prakhar12330@users.noreply.github.com>
2022-08-02 23:32:17 +0530

    Time: 690 ms (45.26%), Space: 230.1 MB (72.35%) - LeetHub

--- 1824-minimum-sideway-jumps/1824-minimum-sideway-jumps.cpp
+++ 1824-minimum-sideway-jumps/1824-minimum-sideway-jumps.cpp
@@ -1,40 +1,48 @@
-//Top Down Approach
+//Bottom Up Approach
 class Solution {
 public:
     
-    int solveRecurr(vector<int>& obstacles,int currlane,int pos,vector<vector<int>>&dp)
+    
+    int solveTab(vector<int>& obstacles)
     {
-        //Base Case-->agar tumhara frog final pos pe pahuch gya tb
-        int n=obstacles.size()-1;
-        if(pos==n)return 0;
+        int n=obstacles.size()-1;//End Pos
+        vector<vector<int>>dp(4,vector<int>(obstacles.size(),-1));
         
-        if(dp[currlane][pos]!=-1)return dp[currlane][pos];
+        dp[0][n]=0;
+        dp[1][n]=0;
+        dp[2][n]=0;
+        dp[3][n]=0;
         
-        //Case01-agar tumhare aage koi obstacle hi nhi hai tb
-        if(obstacles[pos+1] !=currlane)
+        for(int currpos=n-1;currpos>=0;currpos--)
         {
-           return solveRecurr(obstacles,currlane,pos+1,dp); 
-        }
-         //Case01-agar aage koi obstacle hai tb
-        else
-        {
-            int ans=INT_MAX;
-           //up-same pos-down-->teeno jagah me se ek pe kahi bhi move kara skte hai
-            for(int i=1;i<=3;i++)
+            for(int currlane=1;currlane<=3;currlane++)
             {
-                if(currlane!=i && obstacles[pos]!=i)
-                {
-                    ans=min(ans,1+solveRecurr(obstacles,i,pos,dp));
-                }
-            }
-            dp[currlane][pos]=ans;
-            return dp[currlane][pos];
+               if(obstacles[currpos+1] !=currlane)
+                 {
+                    dp[currlane][currpos]=dp[currlane][currpos+1]; 
+                 }
+                else
+                 {
+                     int ans=INT_MAX;
+
+                     for(int i=1;i<=3;i++)
+                        {
+                          if(currlane!=i && obstacles[currpos]!=i)
+                         {
+                            ans=min(ans,1+dp[i][currpos+1]);
+                         }
+                          dp[currlane][currpos]= ans; 
+                         }
             
+               } 
+            }
         }
+        
+        return min(dp[2][0],min(1+dp[1][0],1+dp[3][0]));
+        
     }
     int minSideJumps(vector<int>& obstacles)
     {
-        vector<vector<int>>dp(4,vector<int>(obstacles.size(),-1));
-        return solveRecurr(obstacles,2,0,dp);//frog initially hmara position no.0 pe hai ar lane no.2 pe hai
+        return solveTab(obstacles);//frog initially hmara position no.0 pe hai ar lane no.2 pe hai
     }
 };
