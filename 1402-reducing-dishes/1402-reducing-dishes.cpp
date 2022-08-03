009048d09efe392534e9a5f68c51da4c870805f2
Prakhar Singh <84180549+prakhar12330@users.noreply.github.com>
2022-08-03 23:12:27 +0530

    Time: 47 ms (40.00%), Space: 20.9 MB (18.36%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.cpp
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -1,27 +1,27 @@
-//Top Down Approach
+//Bottom Up Approach
 class Solution {
 public:
-    int solveMem(vector<int>& satisfaction,int index,int time, vector<vector<int>>&dp)
+ 
+    int solveTab(vector<int>& satisfaction)
     {
-         //Base Case-->Jb Index Pointer satisfaction array ke bahar chali jaye
-        if(index==satisfaction.size())
+       int n=satisfaction.size();
+ 
+       vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        
+        for(int index=n-1;index>=0;index--)
         {
-            return 0;
+            for(int time=index;time>=0;time--)
+            {
+                int include=satisfaction[index]*(time+1)+dp[index+1][time+1];
+                int exclude=0+dp[index+1][time];
+                dp[index][time]=max(include,exclude);
+            }
         }
-        
-        if(dp[index][time]!=-1)return dp[index][time];
-        int include=satisfaction[index]*(time+1)+solveMem(satisfaction,index+1,time+1,dp);
-        int exclude=0+solveMem(satisfaction,index+1,time,dp);
-        
-        dp[index][time]=max(include,exclude);
-        return dp[index][time];
+        return dp[0][0];
     }
     int maxSatisfaction(vector<int>& satisfaction)
     {
        sort(satisfaction.begin(),satisfaction.end());
-       int n=satisfaction.size();
-        //Step01
-       vector<vector<int>>dp(n,vector<int>(n,-1));
-       return solveMem(satisfaction,0,0,dp);
+       return solveTab(satisfaction);
     }
 };
