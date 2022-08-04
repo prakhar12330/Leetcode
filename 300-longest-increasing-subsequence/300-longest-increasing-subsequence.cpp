85986daf6d922803361773ff6d135f93981ee3ac
Prakhar Singh <84180549+prakhar12330@users.noreply.github.com>
2022-08-04 22:55:35 +0530

    Time: 994 ms (15.77%), Space: 292.3 MB (8.95%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,28 +1,30 @@
+//Bottom Up Approach
 class Solution {
 public:
-    int solveMem(vector<int>& nums,int currInsertedIdx,int lastInsertedIdx,vector<vector<int>>&dp)
+     int solveTab(vector<int>& nums)
       {
-        if(currInsertedIdx==nums.size())
-        {
-            return 0;
-        }
+        int n=nums.size();
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
         
-        if(dp[currInsertedIdx][lastInsertedIdx+1]!=-1)return dp[currInsertedIdx][lastInsertedIdx+1];
+         for(int currInsertedIdx=n-1;currInsertedIdx>=0;currInsertedIdx--)
+         {
+             for(int lastInsertedIdx=currInsertedIdx-1;lastInsertedIdx>=-1;lastInsertedIdx--)
+             {
+                int include=0;
+                if(lastInsertedIdx==-1 || nums[currInsertedIdx]>nums[lastInsertedIdx])
+                {
+                 include=1+dp[currInsertedIdx+1][currInsertedIdx+1];
+                }  
+                int exclude=0+dp[currInsertedIdx+1][lastInsertedIdx+1];
         
-        int include=0;
-        if(lastInsertedIdx==-1 || nums[currInsertedIdx]>nums[lastInsertedIdx])
-        {
-            include=1+solveMem(nums,currInsertedIdx+1,currInsertedIdx,dp);
-        }
-        int exclude=0+solveMem(nums,currInsertedIdx+1,lastInsertedIdx,dp);
+                dp[currInsertedIdx][lastInsertedIdx+1]= max(include,exclude);
+             }
+         }
+         return dp[0][0];
         
-        dp[currInsertedIdx][lastInsertedIdx+1]= max(include,exclude);
-        return dp[currInsertedIdx][lastInsertedIdx+1];
      }
     int lengthOfLIS(vector<int>& nums)
     {
-        int n=nums.size();
-        vector<vector<int>>dp(n,vector<int>(n+1,-1));
-        return solveMem(nums,0,-1,dp);
+        return solveTab(nums);
     }
 };
