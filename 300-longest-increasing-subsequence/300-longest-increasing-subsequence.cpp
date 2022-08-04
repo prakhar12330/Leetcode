d6d34d24d68e1c78e81077d31fdbe23abb95fea7
Prakhar Singh <84180549+prakhar12330@users.noreply.github.com>
2022-08-04 23:01:17 +0530

    Time: 894 ms (16.92%), Space: 10.6 MB (47.88%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,10 +1,11 @@
-//Bottom Up Approach
+//Space Optimization
 class Solution {
 public:
      int solveTab(vector<int>& nums)
       {
         int n=nums.size();
-        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        vector<int>curr(n+1,0);
+        vector<int>next(n+1,0);
         
          for(int currInsertedIdx=n-1;currInsertedIdx>=0;currInsertedIdx--)
          {
@@ -13,14 +14,15 @@ public:
                 int include=0;
                 if(lastInsertedIdx==-1 || nums[currInsertedIdx]>nums[lastInsertedIdx])
                 {
-                 include=1+dp[currInsertedIdx+1][currInsertedIdx+1];
+                 include=1+next[currInsertedIdx+1];
                 }  
-                int exclude=0+dp[currInsertedIdx+1][lastInsertedIdx+1];
+                int exclude=0+next[lastInsertedIdx+1];
         
-                dp[currInsertedIdx][lastInsertedIdx+1]= max(include,exclude);
+                curr[lastInsertedIdx+1]= max(include,exclude);
              }
+             next=curr;
          }
-         return dp[0][0];
+         return next[0];
         
      }
     int lengthOfLIS(vector<int>& nums)
