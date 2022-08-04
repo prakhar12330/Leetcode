b41794512010b0420bbb5278764492a902c78e20
Prakhar Singh <84180549+prakhar12330@users.noreply.github.com>
2022-08-05 00:07:09 +0530

    Time: 11 ms (91.38%), Space: 10.4 MB (69.86%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,32 +1,30 @@
-//Space Optimization
+//Optimised Approach-->DP+Binary Search
 class Solution {
 public:
-     int solveTab(vector<int>& nums)
-      {
-        int n=nums.size();
-        vector<int>curr(n+1,0);
-        vector<int>next(n+1,0);
-        
-         for(int currInsertedIdx=n-1;currInsertedIdx>=0;currInsertedIdx--)
+     int solveOptimisation(vector<int>& nums)
+     {
+        int n=nums.size(); 
+        if(n==0)return 0;
+        vector<int>ans;
+        ans.push_back(nums[0]);
+         for(int i=1;i<n;i++)
          {
-             for(int lastInsertedIdx=currInsertedIdx-1;lastInsertedIdx>=-1;lastInsertedIdx--)
+             if(nums[i]>ans.back())
              {
-                int include=0;
-                if(lastInsertedIdx==-1 || nums[currInsertedIdx]>nums[lastInsertedIdx])
-                {
-                 include=1+next[currInsertedIdx+1];
-                }  
-                int exclude=0+next[lastInsertedIdx+1];
-        
-                curr[lastInsertedIdx+1]= max(include,exclude);
+                 ans.push_back(nums[i]);
              }
-             next=curr;
+             else
+             {
+                 //Find The Just Bada Element In The Ans
+                 int index=lower_bound(ans.begin(),ans.end(),nums[i])-ans.begin();
+                 ans[index]=nums[i];
+             }
          }
-         return next[0];
+         return ans.size();
         
      }
     int lengthOfLIS(vector<int>& nums)
     {
-        return solveTab(nums);
+        return  solveOptimisation(nums);
     }
 };
