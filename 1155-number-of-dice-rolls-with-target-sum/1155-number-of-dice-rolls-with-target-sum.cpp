c4f03e7223cb93444e436afc11608a49707cbc98
Prakhar Singh <84180549+prakhar12330@users.noreply.github.com>
2022-08-06 01:15:12 +0530

    Time: 39 ms (64.42%), Space: 6.5 MB (72.26%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -1,11 +1,12 @@
-//Bottom Up Approach
+//Space Optimised
 class Solution {
 public:
     #define MOD 1000000007
     int solveTab(int d,int f,int t)
     {
-        vector<vector<int>>dp(d+1,vector<int>(t+1,0));
-        dp[0][0]=1;//after analysing base case
+        vector<int>prev(t+1,0);
+        vector<int>curr(t+1,0);
+        prev[0]=1;//after analysing base case
         
         for(int diceNum=1;diceNum<=d;diceNum++)
         {
@@ -16,13 +17,14 @@ public:
                 {
                     if(targetSum-i>=0)
                     {
-                    ans=(ans%MOD+(dp[diceNum-1][targetSum-i])%MOD)%MOD;
+                    ans=(ans%MOD+(prev[targetSum-i])%MOD)%MOD;
                     }
                 }
-                 dp[diceNum][targetSum]=ans;
+                 curr[targetSum]=ans;
             }
+            prev=curr;
         }
-        return dp[d][t];
+        return prev[t];
     }
     int numRollsToTarget(int n, int k, int target)
     {
