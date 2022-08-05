7b090bf260c82e1082116535ecfa1df924f9844f
Prakhar Singh <84180549+prakhar12330@users.noreply.github.com>
2022-08-06 01:07:05 +0530

    Time: 82 ms (29.40%), Space: 8.5 MB (28.46%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -2,30 +2,31 @@
 class Solution {
 public:
     #define MOD 1000000007
-    int solveMem(int diceNum,int numFaces,int targetSum,vector<vector<int>>&dp)
+    int solveMem(int d,int f,int t)
     {
-        //Base Case 01
-        if(targetSum<0)return 0;
-        //Base Case 02-->agar hmare pass ek bhi dice nhi hai ar hume koi targetSum chahiye 
-        if(diceNum==0 && targetSum != 0)return 0;
-        //Base Case 03-->agar hmare pass dices to hai pr hume koi targetSum nhi chahiye 
-        if(diceNum!=0 && targetSum == 0)return 0;
-        //Base Case 04-->agar hmare pass ek bhi dice nhi hai ar na hi hume koi targetSum banana hai 
-        if(diceNum==0 && targetSum==0)return 1;
+        vector<vector<int>>dp(d+1,vector<int>(t+1,0));
+        dp[0][0]=1;//after analysing base case
         
-        if(dp[diceNum][targetSum]!=-1)return dp[diceNum][targetSum];
-        
-        int ans=0;
-        for(int i=1;i<=numFaces;i++)
+        for(int diceNum=1;diceNum<=d;diceNum++)
         {
-            ans=(ans%MOD+solveMem(diceNum-1,numFaces,targetSum-i,dp)%MOD)%MOD;
+            for(int targetSum=1;targetSum<=t;targetSum++)
+            {
+                int ans=0;
+                for(int i=1;i<=f;i++)
+                {
+                    if(targetSum-i>=0)
+                    {
+                    ans=(ans%MOD+(dp[diceNum-1][targetSum-i])%MOD)%MOD;
+                    }
+                }
+                 dp[diceNum][targetSum]=ans;
+            }
         }
-        dp[diceNum][targetSum]=ans;
-        return dp[diceNum][targetSum];
+        return dp[d][t];
     }
     int numRollsToTarget(int n, int k, int target)
     {
-        vector<vector<int>>dp(n+1,vector<int>(target+1,-1));
-        return solveMem(n,k,target,dp);
+
+        return solveMem(n,k,target);
     }
 };
