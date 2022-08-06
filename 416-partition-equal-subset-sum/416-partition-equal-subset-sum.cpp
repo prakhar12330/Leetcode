6dd448e81edcff365d2e627597579551feb562e8
Prakhar Singh <84180549+prakhar12330@users.noreply.github.com>
2022-08-07 00:29:36 +0530

    Time: 430 ms (33.51%), Space: 85.6 MB (12.94%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -1,23 +1,30 @@
-//Top Down Approach
+//Bottom Up Approach
 class Solution {
 public:
     
-    bool solveRecurr(vector<int>&nums,int n,int idx,int totalSum,vector<vector<int>>& dp)
+    bool solveTab(vector<int>&nums,int target)
     {
-        //Base Case01-->
-        if(totalSum<0)return false;
-        //Base Case02-->
-        if(idx>=n)return false;
-        //Base Case03-->
-        if(totalSum==0)return true;
+       int n =nums.size();
+       vector<vector<int>>dp(n+1,vector<int>(target+1,0));
+       for(int i=0;i<=n;i++)
+       {
+           dp[i][0]=1;
+       }
+       for(int idx=n-1;idx>=0;idx--)
+       {
+        for(int totalSum=0;totalSum<=target;totalSum++)
+         {
+            bool include=0;
+            if(totalSum-nums[idx]>=0)
+            {
+            include=dp[idx+1][totalSum-nums[idx]];
+            }
+            bool exclude=dp[idx+1][totalSum-0];
         
-        if(dp[idx][totalSum]!=-1)return dp[idx][totalSum];
-        
-        bool include=solveRecurr(nums,n,idx+1,totalSum-nums[idx],dp);
-        bool exclude=solveRecurr(nums,n,idx+1,totalSum-0,dp);
-        
-        dp[idx][totalSum]=include or exclude;
-        return dp[idx][totalSum];
+            dp[idx][totalSum]=include or exclude;
+         }
+       }
+        return dp[0][target];
     }
     bool canPartition(vector<int>& nums)
     {
@@ -31,8 +38,6 @@ public:
         if(totalSum & 1)return false;//if total sum of the array is odd
         //if even
         int target=totalSum/2;
-        
-        vector<vector<int>>dp(n,vector<int>(target+1,-1));
-        return solveRecurr(nums,n,0,target,dp);
+        return solveTab(nums,target);
     }
 };
