73e2ab69bbe55c6f83b869a638a3ab77f723518c
Prakhar Singh <84180549+prakhar12330@users.noreply.github.com>
2022-08-07 00:42:03 +0530

    Time: 267 ms (59.39%), Space: 11.1 MB (81.89%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -1,15 +1,17 @@
-//Bottom Up Approach
+//Space Optimised
 class Solution {
 public:
     
     bool solveTab(vector<int>&nums,int target)
     {
        int n =nums.size();
-       vector<vector<int>>dp(n+1,vector<int>(target+1,0));
-       for(int i=0;i<=n;i++)
-       {
-           dp[i][0]=1;
-       }
+        
+        vector<int>curr(target+1,0);
+        vector<int>next(target+1,0);
+        
+        curr[0]=1;
+        next[0]=1;
+       
        for(int idx=n-1;idx>=0;idx--)
        {
         for(int totalSum=0;totalSum<=target;totalSum++)
@@ -17,14 +19,15 @@ public:
             bool include=0;
             if(totalSum-nums[idx]>=0)
             {
-            include=dp[idx+1][totalSum-nums[idx]];
+            include=next[totalSum-nums[idx]];
             }
-            bool exclude=dp[idx+1][totalSum-0];
+            bool exclude=next[totalSum-0];
         
-            dp[idx][totalSum]=include or exclude;
+            curr[totalSum]=include or exclude;
          }
+           next=curr;
        }
-        return dp[0][target];
+        return next[target];
     }
     bool canPartition(vector<int>& nums)
     {
