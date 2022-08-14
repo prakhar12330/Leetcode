14ec5cd6599ddea547696ac0518462d5e7f22999
Prakhar Singh <84180549+prakhar12330@users.noreply.github.com>
2022-08-14 19:37:22 +0530

    Time: 432 ms (23.92%), Space: 14.2 MB (79.98%) - LeetHub

---
+++ 947-most-stones-removed-with-same-row-or-column/947-most-stones-removed-with-same-row-or-column.cpp
@@ -0,0 +1,36 @@
+class Solution {
+    vector<int>parent;
+    int find(int x) {
+        return parent[x] == x ? x : find(parent[x]);
+    }
+public:
+    int removeStones(vector<vector<int>>& stones) {
+        
+        if (stones.empty()) return 0;
+        
+        int m = stones.size();
+        
+        parent.resize(m, 0);
+        for (int i = 0; i < m; i++)
+            parent[i] = i;
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < i; j++) {
+                if (stones[i][0] == stones[j][0] || stones[i][1] == stones[j][1]) {
+                    int x = find(i);
+                    int y = find(j);
+                    if (x != y)
+                        parent[y] = x;
+                }
+            }
+        }
+        
+        int ans = 0;
+        for (int i = 0; i < m; i++) {
+            if (parent[i] == i)
+                ans++;
+        }
+        
+        return m-ans;
+    }
+};
