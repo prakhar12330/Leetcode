4b66f50107f718c04836e96be30867627f8be113
Prakhar Singh <84180549+prakhar12330@users.noreply.github.com>
2022-08-14 19:41:53 +0530

    Time: 303 ms (45.29%), Space: 14.2 MB (87.22%) - LeetHub

--- 947-most-stones-removed-with-same-row-or-column/947-most-stones-removed-with-same-row-or-column.cpp
+++ 947-most-stones-removed-with-same-row-or-column/947-most-stones-removed-with-same-row-or-column.cpp
@@ -1,24 +1,25 @@
 class Solution {
-
+public:
     int find(int x,vector<int>&parent)
     {
         if(x==parent[x])return x;
         return find(parent[x],parent);
     }
-public:
-    int removeStones(vector<vector<int>>& stones) {
+    int removeStones(vector<vector<int>>& stones) 
+    {
+        int totalStones=stones.size();
+        vector<int>parent(totalStones,0);
+        for(int i=0;i<totalStones;i++)
+        {
+            parent[i]=i;
+        }
         
-        if (stones.empty()) return 0;
-        
-        int m = stones.size();
-        
-        vector<int>parent(m, 0);
-        for (int i = 0; i < m; i++)
-            parent[i] = i;
-        
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < i; j++) {
-                if (stones[i][0] == stones[j][0] || stones[i][1] == stones[j][1]) {
+        for (int i = 0; i < totalStones; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                if (stones[i][0] == stones[j][0] || stones[i][1] == stones[j][1])
+                {
                     int x = find(i,parent);
                     int y = find(j,parent);
                     if (x != y)
@@ -28,11 +29,12 @@ public:
         }
         
         int ans = 0;
-        for (int i = 0; i < m; i++) {
+        for (int i = 0; i < totalStones; i++) 
+        {
             if (parent[i] == i)
                 ans++;
         }
         
-        return m-ans;
+        return totalStones-ans;
     }
 };
