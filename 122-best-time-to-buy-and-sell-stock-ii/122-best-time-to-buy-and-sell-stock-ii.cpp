3aed766da5deeca5870989184b092b4bb988de03
Prakhar Singh <84180549+prakhar12330@users.noreply.github.com>
2022-08-22 02:22:43 +0530

    Time: 11 ms (59.87%), Space: 13 MB (58.33%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,11 +1,12 @@
-//Bottom Up Approach
+//Space Optimization
 class Solution {
 public:
     int solveRecurr(vector<int>& prices)
     {
         int n=prices.size();
         //Step-->01
-        vector<vector<int>>dp(n+1,vector<int>(2,0));
+        vector<int>curr(2,0);
+        vector<int>next(2,0);
         //Step-->02
         for(int idx=n-1;idx>=0;idx--)
         {
@@ -14,21 +15,22 @@ public:
                int profit=0;
                if(buy==1)//Buy is a parameter
                {
-                  int buyKaro=-prices[idx]+dp[idx+1][0];
-                  int skipKaro=0+dp[idx+1][1];
+                  int buyKaro=-prices[idx]+next[0];
+                  int skipKaro=0+next[1];
                    profit=max(buyKaro,skipKaro);
                }
                 else
               {
-                  int sellKaro=+prices[idx]+dp[idx+1][1];
-                  int skipKaro=0+dp[idx+1][0];
+                  int sellKaro=+prices[idx]+next[1];
+                  int skipKaro=0+next[0];
                   profit=max(sellKaro,skipKaro);
               }
-               dp[idx][buy]=profit;
+               curr[buy]=profit;
              }
+            next=curr;
         }
         //Step-->03
-        return dp[0][1];
+        return next[1];
     }
     int maxProfit(vector<int>& prices) 
     {
