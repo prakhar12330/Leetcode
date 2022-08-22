b7d5de8c99cedd6baf3cf23597fb5a6f19be91e4
Prakhar Singh <84180549+prakhar12330@users.noreply.github.com>
2022-08-22 17:57:34 +0530

    Time: 18 ms (55.86%), Space: 10.9 MB (83.82%) - LeetHub

---
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int solveRecurr(vector<int>& prices,int k)
+    {
+        int n=prices.size();
+        //Step01
+        vector<vector<int>>curr(2,vector<int>(k+1,0));
+        vector<vector<int>>next(2,vector<int>(k+1,0));
+        for(int idx=n-1;idx>=0;idx--)
+        {
+            for(int buy=0;buy<=1;buy++)
+            {
+              for(int limit=1;limit<=k;limit++)
+              {
+                  int profit=0;
+                  if(buy==1)//Buy is a parameter
+                  {
+                     int buyKaro=-prices[idx]+next[0][limit];
+                     int skipKaro=0+next[1][limit];
+                     profit=max(buyKaro,skipKaro);
+                  }
+                  else
+                  {
+                   int sellKaro=+prices[idx]+next[1][limit-1];
+                   int skipKaro=0+next[0][limit];
+                   profit=max(sellKaro,skipKaro);
+                  }
+                 
+                curr[buy][limit]=profit;
+                                               
+               }
+            next=curr;
+            }
+        }
+        return next[1][k];
+    }    
+
+    int maxProfit(int k, vector<int>& prices)
+    {
+        return solveRecurr(prices,k);
+    }
+};
