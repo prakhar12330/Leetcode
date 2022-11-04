e1b4465f4948ced11c21c3ab3a1c4f534eb5922a
Prakhar Singh <84180549+prakhar12330@users.noreply.github.com>
2022-11-04 23:42:36 +0530

    Time: 27 ms (84.82%), Space: 5.8 MB (88.92%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -5,11 +5,11 @@ public:
         if(n==0) return false;
         if(n==1) return true;
         
-        while(n%3 ==0)
+        if(n%3 !=0)
         {
-            n /= 3;
+            return false;
         }
         
-        return n==1;
+        return isPowerOfThree(n/3);
     }
 };
