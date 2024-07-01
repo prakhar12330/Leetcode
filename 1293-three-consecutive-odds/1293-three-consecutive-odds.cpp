566ac03044d8da79b9305ae9d4f2590c03d75a1f
Prakhar Singh <84180549+prakhar12330@users.noreply.github.com>
2024-07-01 08:09:47 +0530

    Time: 0 ms (100%), Space: 10.6 MB (45.76%) - LeetHub

--- 1293-three-consecutive-odds/1293-three-consecutive-odds.cpp
+++ 1293-three-consecutive-odds/1293-three-consecutive-odds.cpp
@@ -1,16 +1,25 @@
+/*
+Approach1: Using counter
+Approach2: Using sliding window
+*/
+
 class Solution {
 public:
     bool threeConsecutiveOdds(vector<int>& arr)
     {
-        int count=0;
-        for(int i=0;i<arr.size();i++){
-            if(arr[i]%2!=0){
-                count++;
-                if(count==3)return true;
-            }
-            else{
-                count=0;
-            }
+        int window=0;
+        for(int i=0;i<3 && i<arr.size();i++)
+        {
+            window+=arr[i]%2;
+        }
+        if (window == 3) {
+            return true;
+        }
+        for(int i=3;i<arr.size();i++)
+        {
+            window+=arr[i]%2;
+            window-=arr[i-3]%2;
+            if(window==3){return true;}
         }
         return false;
     }
